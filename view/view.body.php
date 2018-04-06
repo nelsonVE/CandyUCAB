71b9e51911f2c05681536fecfeafdd5dc9eebf7d
Nelson <nelsonmggomes@gmail.com>
2018-04-05 20:12:00 -0400

    Primera etapa de la interfaz (mockup semi-funcional)

---
+++ view/view.body.php
@@ -0,0 +1,75 @@
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12 bg-content-main">
+			<div class="text-center mt-5">
+        <h2 class="text-white">¿Qué esperas para registrarte?</h2><hr>
+        <div class="row">
+          <div class="col-md-2 col-sm-0"></div>
+          <div class="col-md-8 col-sm-0">
+            <form>
+              <div class="form-group row">
+                <input type="text" class="form-control col-center" id="nombre" placeholder="Nombre">
+              </div>
+              <div class="form-group row">
+                <input type="text" class="form-control col-center" id="apellido" placeholder="Apellido">
+              </div>
+              <div class="form-group row">
+                <input type="email" class="form-control col-center" id="email" placeholder="Email">
+              </div>
+              <button type="button" class="btn btn-afil mb-5">
+                ¡Afíliate a nosotros!
+              </button>
+            </form>
+          </div>
+          <div class="col-md-2 col-sm-0"></div>
+        </div>
+			</div>
+		</div>
+		<div class="col-md-12 bg-content p-5 aos-item">
+      <h1 class="text-center tit-main">¿Por qué escojernos?</h1><br>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 p-3" style="background-color:#ffeeee; word-wrap: break-word;">
+            <h2 class="text-center tit-main">
+              <i class="far fa-handshake" style="font-size:100px;"></i>
+              <br><br>
+              Profesionalismo
+              <br><hr>
+            </h2>
+            <p class="text-justify">Candy UCAB es un grupo multinacional que se especializa en la
+            elaboración de caramelos. Sus productos se elaboran bajo los más altos
+            estándares de calidad en 47 plantas industriales ubicadas en
+            Latinoamérica y desarrolla marcas líderes que disfrutan consumidores
+            de todo el mundo.</p>
+          </div>
+          <div class="col-md-4 p-3" style="background-color:#ffc1c1; color:#7a7a7a; word-wrap: break-word;">
+            <h2 class="text-center">
+              <i class="far fa-compass" style="font-size:100px;"></i>
+              <br><br>
+              Accesibilidad
+              <br><hr>
+            </h2>
+            <p class="text-justify">Con oficinas comerciales ubicadas en América, Europa y Asia, Candy
+            UCAB es el grupo venezolano con la mayor cantidad de mercados abiertos
+            en el mundo, llegando con sus productos a más de 120 países.
+            </p>
+          </div>
+          <div class="col-md-4 p-3" style="background-color:#ea6485; color:white; word-wrap: break-word;">
+            <h2 class="text-center">
+              <i class="far fa-star" style="font-size:100px;"></i>
+              <br><br>
+              Variedad
+              <br><hr>
+            </h2>
+            <p class="text-justify">Poseemos actualmente una gran variedad de golosinas a la venta para
+              nuestro público, con un exquisito sabor, dulzura y además extremadamente agradables para
+              el paladar, lo cual nos diferencia y nos hace tan únicos en el mercado.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+		<div class="col-md-12 bg-trans"></div>
+		<div class="col-md-12 bg-content"></div>
+	</div>
+</div>
---
+++ view/view.header.php
@@ -0,0 +1,35 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-candy">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Twemoji2_1f36c.svg/512px-Twemoji2_1f36c.svg.png" width="32" height="32" alt="puta tarea">
+      CandyUcab
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Inicio</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Tienda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contáctanos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Acerca de nostros</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item mr-3">
+          <a class="nav-link" href="#">Login</span></a>
+        </li>
+        <li class="reg-button">
+          <a class="nav-link" href="#">Regístrate</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
